7a3ad7e206a3e7a734de22b224422c874fef0d15
Neeraj Handa <45884013+neerajhanda@users.noreply.github.com>
2022-09-14 17:26:26 +0000

    Reusing a semantic model object after first creation

--- src/Analysis/Codelyzer.Analysis.Build/Models/SourceFileBuildResult.cs
+++ src/Analysis/Codelyzer.Analysis.Build/Models/SourceFileBuildResult.cs
@@ -10,9 +10,36 @@ namespace Codelyzer.Analysis.Build
     {
         private readonly Compilation _prePortCompilation;
         private readonly Compilation _compilation;
+
+        private SemanticModel _prePortSemanticModel;
+        private SemanticModel _semanticModel;
         public SyntaxTree SyntaxTree { get; set; }
-        public SemanticModel SemanticModel => _compilation?.GetSemanticModel(SyntaxTree);
-        public SemanticModel PrePortSemanticModel => _prePortCompilation?.GetSemanticModel(SyntaxTree);
+        public SemanticModel SemanticModel
+        {
+            get
+            {
+                if (_semanticModel == null)
+                {
+                    _semanticModel = _compilation?.GetSemanticModel(SyntaxTree);
+                }
+
+                return _semanticModel;
+            }
+        }
+
+        public SemanticModel PrePortSemanticModel
+        {
+            get
+            {
+                if (_prePortSemanticModel == null)
+                {
+                    _prePortSemanticModel = _prePortCompilation?.GetSemanticModel(SyntaxTree);
+                }
+
+                return _prePortSemanticModel;
+            }
+        }
+
         public string SourceFileFullPath { get; set; }
         public string SourceFilePath { get; set; }
         public SyntaxGenerator SyntaxGenerator { get; set; }
